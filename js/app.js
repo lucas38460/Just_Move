e7cfea3dedd2bfcc9d53a1fe9449efa7d426c91d
Lux <92374048+lucas38460@users.noreply.github.com>
2022-06-08 11:04:57 +0200

    fin première partie

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-const leftDiv = document.querySelector('.leftDiv');
-const rightDiv = document.querySelector('.rightDiv');
+const leftDiv = document.querySelector("#leftDiv");
+const rightDiv = document.querySelector("#rightDiv");
+
 const divsChild = leftDiv.children;
 
 for (const div of divsChild) {
-    div.addEventListener('click', () => {
-        if (div.parentElement.id === "leftDiv") {
-            rightDiv.appendChild(div);
-        } else {
-            leftDiv.appendChild(div);
-        }
-    });
-};
+  div.addEventListener("click", () => {
+      if (div.parentElement.id === "leftDiv") {
+        rightDiv.appendChild(div);
+      } else {
+        leftDiv.appendChild(div);
+      }
+  });
+}
